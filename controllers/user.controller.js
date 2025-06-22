fe687791347029fc71ef8e7ba90954706c185f2c
ShivamMishraa25 <shivam.m4464@gmail.com>
2025-06-23 01:53:58 +0530

    mongoose is added again

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,11 @@
 import userModel from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 export async function userRegister(req, res) {
     try {
-        const newUser = await userModel.create(req.body);
+        let {fullName, email, password} = req.body;
+        password = bcrypt.hashSync(password, 10);
+        const newUser = await userModel.create({fullName, email, password});
         res.status(201).json(newUser);
     } catch (err) {
         res.status(500).json({error: err.message});
