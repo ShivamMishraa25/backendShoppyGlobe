37c212374b84937faf7e59aed069ba82c60295ad
ShivamMishraa25 <shivam.m4464@gmail.com>
2025-06-23 02:09:24 +0530

    register user done fully

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,8 +4,11 @@ import bcrypt from "bcrypt";
 export async function userRegister(req, res) {
     try {
         let {fullName, email, password} = req.body;
-        password = bcrypt.hashSync(password, 10);
-        const newUser = await userModel.create({fullName, email, password});
+        if(await userModel.findOne({email: email})) {
+            return res.status(409).send("email is already registered");
+        }
+        let hashedPassword = bcrypt.hashSync(password, 10);
+        const newUser = await userModel.create({fullName, email, hashedPassword});
         res.status(201).json(newUser);
     } catch (err) {
         res.status(500).json({error: err.message});
